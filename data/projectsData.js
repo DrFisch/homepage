432023d5a63c2407969c11eb836fab94aa906a87
Lukas Sponsel <lukassponsel5@gmail.com>
2024-10-24 15:05:14 +0200

    neues projekt hinzugefügt

--- data/projectsData.js
+++ data/projectsData.js
@@ -135,6 +135,133 @@ export const projectsData = [
 			// ],
 		},
 	},
+	{
+		id: 2,
+		title: 'Spo Blog',
+		url: 'budget-tracker-54473.web.app',
+		category: 'Blog',
+		img: '/images/mobile-project-2.jpg',
+		ProjectHeader: {
+			title: 'Spo Blog',
+			publishDate: '24.10.2024',
+			tags: 'UI / Frontend',
+		},
+		ProjectImages: [
+			// {
+			// 	id: uuidv4(),
+			// 	title: 'Google Health Platform',
+			// 	img: '/images/ui-project-1.jpg',
+			// },
+			// {
+			// 	id: uuidv4(),
+			// 	title: 'Google Health Platform',
+			// 	img: '/images/web-project-2.jpg',
+			// },
+			// {
+			// 	id: uuidv4(),
+			// 	title: 'Google Health Platform',
+			// 	img: '/images/mobile-project-2.jpg',
+			// },
+		],
+		ProjectInfo: {
+			ClientHeading: 'Über das Projekt',
+			CompanyInfo: [
+				{
+					id: uuidv4(),
+					title: 'Name',
+					details: 'Spo Blog',
+				},
+				{
+					id: uuidv4(),
+					title: 'Services',
+					details: 'Web Development',
+				},
+				{
+					id: uuidv4(),
+					title: 'Website',
+					details: 'budget-tracker-54473.web.app',
+				},
+				// {
+				// 	id: uuidv4(),
+				// 	title: 'Phone',
+				// 	details: '555 8888 888',
+				// },
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, natus! Quibusdam enim quod in esse, mollitia molestias incidunt quas ipsa accusamus veniam.',
+			Technologies: [
+				{
+					title: 'Tools & Technologien',
+					techs: [
+						'HTML',
+						'CSS',
+						'React',
+						'JavaScript',
+						'NextJS',
+						'TailwindCSS',
+						'AdobeXD',
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Challenge',
+			ProjectDetails: [
+				{
+					id: uuidv4(),
+					details:
+						'Link: budget-tracker-54473.web.app',
+				},
+				{
+					id: uuidv4(),
+					details:
+						'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+				},
+				{
+					id: uuidv4(),
+					details:
+						'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+				},
+				{
+					id: uuidv4(),
+					details:
+						'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+				},
+			],
+			SocialSharingHeading: '',
+			// SocialSharing: [
+			// 	{
+			// 		id: uuidv4(),
+			// 		name: 'Twitter',
+			// 		icon: <FiTwitter />,
+			// 		url: 'https://twitter.com/realstoman',
+			// 	},
+			// 	{
+			// 		id: uuidv4(),
+			// 		name: 'Instagram',
+			// 		icon: <FiInstagram />,
+			// 		url: 'https://instagram.com/realstoman',
+			// 	},
+			// 	{
+			// 		id: uuidv4(),
+			// 		name: 'Facebook',
+			// 		icon: <FiFacebook />,
+			// 		url: 'https://facebook.com/',
+			// 	},
+			// 	{
+			// 		id: uuidv4(),
+			// 		name: 'LinkedIn',
+			// 		icon: <FiLinkedin />,
+			// 		url: 'https://linkedin.com/',
+			// 	},
+			// 	{
+			// 		id: uuidv4(),
+			// 		name: 'Youtube',
+			// 		icon: <FiYoutube />,
+			// 		url: 'https://www.youtube.com/c/StomanStudio',
+			// 	},
+			// ],
+		},
+	},
 	// {
 	// 	id: 2,
 	// 	title: 'Phoenix Digital Agency',
